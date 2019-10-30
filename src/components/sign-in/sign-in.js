7492e0216de99f032ac6f85ec97c9812575935a8
taslimy <taslimer@gmail.com>
2019-10-30 18:48:51 -0400

    added the signin medthod

--- src/components/sign-in/sign-in.js
+++ src/components/sign-in/sign-in.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import FormInput from '../form-input/form-input';
 import CustomButton from '../custom-button/custom-button';
 
+import { auth } from '../../firebase/firebase.utils';
+
 import { signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.scss';
@@ -17,12 +19,18 @@ export default class SignIn extends Component {
     };
   }
 
-  handleSubmit = e => {
+  handleSubmit = async e => {
     e.preventDefault();
-    this.setState({
-      email: '',
-      password: ''
-    });
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({
+        email: '',
+        password: ''
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handleChange = e => {
