acfacfc16eab39af9f488912e4fd0aba0483bc67
taslimy <taslimer@gmail.com>
2019-10-31 14:14:56 -0400

    carticon and dropdown components added also added redux for cart

---
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import CustomButton from '../custom-button/custom-button';
+
+import './cart-dropdown.scss';
+
+const CartDropdown = () => (
+  <div className='cart-dropdown'>
+    <div className='cart-items' />
+    <CustomButton>Go To Checkout</CustomButton>
+  </div>
+);
+
+export default CartDropdown;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon';
+import CartDropDown from '../cart-dropdown/cart-dropdown';
 
 import './header.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <p className='logo'>eCommerce</p>
@@ -27,12 +29,15 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropDown />}
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
