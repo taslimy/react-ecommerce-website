a31c23b2b67e5e4fcac5631ae9b2a1691a345aa2
taslimy <taslimer@gmail.com>
2019-11-01 13:37:27 -0400

    add item and remove item from checkout page is now working

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CardActionTypes from './cart.types';
-import { addItemToCart } from './cart.util';
+import { addItemToCart, removeItemFromCart } from './cart.util';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -17,6 +17,18 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
+      };
+    case CardActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
+      };
+    case CardActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload.id
+        )
       };
     default:
       return state;
