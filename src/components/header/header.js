1da6ce8cea0430e7248ad025679c5ee0ec834144
taslimy <taslimer@gmail.com>
2019-11-01 18:22:08 -0400

    some quick fixes / changes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,19 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-
 import { auth } from '../../firebase/firebase.utils';
+
 import CartIcon from '../cart-icon/cart-icon';
 import CartDropDown from '../cart-dropdown/cart-dropdown';
+
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
+
+import { ReactComponent as Logo } from '../../assets/logo.svg';
 
 import './header.scss';
 
 const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
-      <p className='logo'>eCommerce</p>
+      <Logo className='logo' /> 
+      <span className='logo-text'> React eCommerce </span>
     </Link>
     <div className='options'>
       <Link className='option' to='/shop'>
--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -5,7 +5,6 @@ import CollectionsOverview from '../../components/collections-overview/collectio
 import CollectionPage from '../collection/collection';
 
 const ShopPage = ({ match }) => {
-  console.log(match);
   return (
     <div className='shop-page'>
       <Route exact path={`${match.path}`} component={CollectionsOverview} />
